9bbd3ed5707593666bfc0617281e27770318be55
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-07-18 17:55:02 +0100

    changes in t_philo and t_table structs

--- philo/philo.h
+++ philo/philo.h
@@ -24,37 +24,36 @@
 typedef pthread_mutex_t	t_mtx;
 typedef struct s_table	t_table;
 
-typedef struct s_fork
-{
-	t_mtx	fork;
-	int		fork_id;
-}				t_fork;
-
 typedef struct s_philo
 {
 	int			philo_id;
-	long		meal_counter;
-	bool		full; //A flag or boolean
+	long		meals_counter;
+	bool		full;
 	long		last_meal_time;
-	t_fork		*first_fork;
-	t_fork		*second_fork;
+	t_mtx		*first_fork;
+	t_mtx		*second_fork;
 	pthread_t	thread_id;
+	t_mtx		philo_mutex;
 	t_table		*table;
 }				t_philo;
 
 typedef struct s_table
 {
-	long	num_philo;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	num_each_philo_must_eat;
-	long	start_simulation;
-	bool	end_simulation;
-	bool	all_threads_ready;
-	t_mtx	table_mutex;
-	t_fork	*forks;
-	t_philo	*philos;
+	long		num_philo;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		nnbr_limit_meals;
+	long		start_simulation;
+	long		threads_running_nbr;
+	long		philos_full;
+	bool		end_simulation;
+	bool		all_threads_ready;
+	t_mtx		*forks;
+	t_mtx		table_mutex;
+	t_mtx		write_mutex;
+	pthread_t	monitor;
+	t_philo		*philos;
 }				t_table;
 
 //----------parsing.c--------------
