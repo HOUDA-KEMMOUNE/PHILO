0073a91815a7d3034600c4ba2900e835eb7071d2
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2025-07-19 00:13:18 +0100

    starting the dinner function

--- philo/dinner.c
+++ philo/dinner.c
@@ -12,4 +12,22 @@
 
 #include "philo.h"
 
+void	*one_philo(void *data)
+{
+	t_philo	*philo;
 
+	philo = (t_philo *)data;
+	
+}
+
+void	dinner_start(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	if (table->nbr_limit_meals == 0)
+		return ;
+	else if (table->num_philo == 1)
+		pthread_create(&table->philos[0].thread_id,
+			NULL, one_philo, &table->philos[0]);
+}
